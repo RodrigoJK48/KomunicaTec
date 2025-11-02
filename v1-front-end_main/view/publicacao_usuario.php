3ce6a66db387b479fab9a6b2289eacb4c391c4d4
RodrigoJK48 <rokassahara@gmail.com>
2025-11-02 00:01:17 -0300

    Atualização de Visualização de Publicação

--- v1-front-end_main/view/publicacao_usuario.php
+++ v1-front-end_main/view/publicacao_usuario.php
@@ -1,10 +1,10 @@
-<?php // Página de exibição de publicações feitas pelo usuário
-
+<?php // Página de exibição de publicações feitas por usuários
 require_once '../controller/controlar_publicacao.php'; // Requer arquivo controlador para exibição
 
-$publicados = $publicacao->consultar_todos(); // Executa função de consulta
+// Executa funções de consulta, recebe dados de publicações e categorias
+$publicados = $publicacao->consultar_todos();
+$categorias = $categoria->consultar();
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -24,7 +24,7 @@ $publicados = $publicacao->consultar_todos(); // Executa função de consulta
                 <img src="imagens/logo1.png" alt="Logo KomunikaTec" class="img-logo">
             </figure>
             <div class="botao-navegacao">
-                <button class="botao" onclick="location.href='index.html'">Home</button>
+                <button class="botao" onclick="location.href='publicacao.php'">Home</button>
                 <button class="botaoent" onclick="location.href='login.html'">Entrar</button>
                 <button class="botao-especial" onclick="location.href='register.html'">Cadastrar</button>
             </div>
@@ -52,26 +52,52 @@ $publicados = $publicacao->consultar_todos(); // Executa função de consulta
 
             <!-- Filtro -->
             <div class="filtro">
-                <label><input type="checkbox"> Vagas</label>
-                <label><input type="checkbox"> Cursos</label>
-                <label><input type="checkbox"> Eventos</label>
-                <label><input type="checkbox"> Outros</label>
+                <?php
+				foreach ($categorias as $opcao)
+				{
+					echo "<label><input type='checkbox'> ".$opcao['nome_categoria']."</label>";
+				}
+				?>
             </div>
+            
+            <button class="editar" onclick="location.href='form_publicacao.php'">+ Adicionar</button>
 
             <!-- Lista de publicações -->
             <?php foreach($publicados as $publicado): ?>
             <div class="publicacao-card">
                 <div class="cabecalho">
                     <h2><?= $publicado["titulo"] ?></h2>
+                    <?= $publicado["nome_categoria"] ?>
                     <span>
-                        Publicado em <?= $publicado["data_de_publicacao"] ?>
-                    </span>
+                        Publicado em <?= date("d/m/Y H:i", strtotime($publicado["data_de_publicacao"])) // Formato da data, converte valor string para tempo ?>
+                    	<br>Valido até <?= date("d/m/Y", strtotime($publicado["data_de_expiracao"])) ?></span>
                 </div>
                 <!-- "whitespace: pre-line" converte quebras de linha para HTML -->
                 <p style="white-space: pre-line;"><?= $publicado["descricao"] ?></p>
+                <?php
+                if (isset($publicado["endereco_link"]) && $publicado["endereco_link"] != null)
+				{
+					echo '<a href="'.$publicado["endereco_link"].'" class="link">'.$publicado["endereco_link"].'</a>';
+				}
+                ?>
                 <div class="acoes">
-                    <button class="editar" onclick="location.href='edit_publi.html'">Editar</button>
-                    <button class="excluir">Excluir</button>
+                    <?php
+                    echo "<button class='editar'
+                        onclick='atualizar(
+                            ".$publicado["id_publicacao"].",
+                            \"".$publicado["titulo"]."\",
+                            \"".$publicado["descricao"]."\",
+                            \"".$publicado["data_de_publicacao"]."\",
+                            \"".$publicado["data_de_expiracao"]."\",
+                            \"".$publicado["endereco_link"]."\",
+                            ".$publicado["id_categoria"].",
+                            ".$publicado["id_link_publicacao"]."
+                        )'
+                    >
+                        Editar
+                    </button>";
+                    echo "<button class='excluir' onclick='apagar(".$publicado["id_publicacao"].")'>Excluir</button>";
+                    ?>
                 </div>
             </div>
             <?php endforeach; ?>
@@ -89,7 +115,7 @@ $publicados = $publicacao->consultar_todos(); // Executa função de consulta
                     Estudantes do GT1 e G3E estão convidados.
                 </p>
                 <div class="acoes">
-                   <button class="editar" onclick="location.href='edit_publi.html'">Editar</button>
+                   <button class="editar" onclick="location.href='form_publicacao.html'">Editar</button>
                     <button class="excluir">Excluir</button>
                 </div>
             </div>
@@ -104,7 +130,7 @@ $publicados = $publicacao->consultar_todos(); // Executa função de consulta
                     Interessados, por favor, clicar no link.
                 </p>
                 <div class="acoes">
-                    <button class="editar" onclick="location.href='edit_publi.html'">Editar</button>
+                    <button class="editar" onclick="location.href='form_publicacao.html'">Editar</button>
                     <button class="excluir">Excluir</button>
                 </div>
             </div>
@@ -115,5 +141,70 @@ $publicados = $publicacao->consultar_todos(); // Executa função de consulta
     <footer>
         <p>© 2025 - KomunicaTec</p>
     </footer>
+
+
+
+    <script>
+        function atualizar(
+			ID_PUBLICACAO, TITULO, DESCRICAO, DATA_DE_PUBLICACAO, DATA_DE_EXPIRACAO, ENDERECO_LINK, ID_CATEGORIA, ID_LINK_PUBLICACAO
+		){
+            const dados = {
+				id_publicacao:		ID_PUBLICACAO,
+				titulo:				TITULO,
+				descricao:			DESCRICAO,
+				data_de_publicacao:	DATA_DE_PUBLICACAO,
+				data_de_expiracao:	DATA_DE_EXPIRACAO,
+				endereco_link:		ENDERECO_LINK,
+				id_categoria:		ID_CATEGORIA,
+				id_link_publicacao:	ID_LINK_PUBLICACAO
+			};
+
+    		// Cria formulário oculto para enviar dados e ação ao formulário de edição
+    		const FORM_ATUALIZACAO = document.createElement('form'); // Declara elemento de formulário
+    		FORM_ATUALIZACAO.method = 'POST'; // Estabelece método de requisição como POST
+    		FORM_ATUALIZACAO.action = 'form_publicacao.php?acao=atualizacao'; // Destino dos dados enviados
+			FORM_ATUALIZACAO.style.display = 'none'; // Exibição oculta, não aparece na tela do usuário
+
+			for (const dado in dados) // Para cada linha de "dados"
+			{
+				if (dados.hasOwnProperty(dado))
+				{
+       				// Campos de recebimento de dados
+        			const ENTRADA = document.createElement('input'); // Declara elemento input
+        			ENTRADA.type = 'hidden'; // Tipo oculto, não aparece na tela do usuário
+        			ENTRADA.name = dado; // Estabelece nome para recebimento do controlador
+        			ENTRADA.value = dados[dado]; // Aplica valor recebido (id de publicação)
+        			FORM_ATUALIZACAO.appendChild(ENTRADA); // Estabelece herança com o formulário
+				}
+			}
+
+        	document.body.appendChild(FORM_ATUALIZACAO); // Estabelece herança com o body
+        	FORM_ATUALIZACAO.submit(); // Envia o formulário
+        }
+
+
+
+        function apagar(id_publicacao)
+		{
+    		if (confirm("Deseja realmente excluir esta publicação?")) // Alerta de confirmação
+			{
+    		    // Cria formulário oculto para enviar código e ação ao controlador
+    		    const FORM_EXCLUSAO = document.createElement('form'); // Declara elemento de formulário
+    		    FORM_EXCLUSAO.method = 'POST'; // Estabelece método de requisição como POST
+    		    FORM_EXCLUSAO.action = '../controller/controlar_publicacao.php?acao=excluir'; // Destino dos dados enviados
+				FORM_EXCLUSAO.style.display = 'none'; // Torna oculto
+        
+        		// Campo de recebimento de id_publicacao
+        		const ID_EXCLUSAO = document.createElement('input'); // Declara elemento input
+        		ID_EXCLUSAO.type = 'hidden'; // Tipo oculto, não aparece na tela do usuário
+        		ID_EXCLUSAO.name = 'id_publicacao'; // Estabelece nome para recebimento do controlador
+        		ID_EXCLUSAO.value = id_publicacao; // Aplica valor recebido (id de publicação)
+        		FORM_EXCLUSAO.appendChild(ID_EXCLUSAO); // Estabelece herança com o formulário
+
+        		document.body.appendChild(FORM_EXCLUSAO); // Estabelece herança com o body
+        		FORM_EXCLUSAO.submit(); // Envia o formulário
+    		}
+		}
+    </script>
 </body>
 </html>
